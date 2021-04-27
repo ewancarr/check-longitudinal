bbd1dac3f05777761ebc6473e065b9a5169c2f88
Ewan Carr <ewan.carr@kcl.ac.uk>
2021-04-27 11:35:51 +0100

    Add WIP script to extract contextual data

---
+++ analysis/cleaning/contextual_data.R
@@ -0,0 +1,36 @@
+# Title:        Import contextual data
+# Author:       Ewan Carr
+# Started:      2021-04-22
+
+library(tidyverse)
+library(here)
+library(janitor)
+library(jsonlite)
+
+gov_uk <- "https://api.coronavirus.data.gov.uk/v2/data?areaType=overview&metric=hospitalCases&metric=newAdmissions&metric=newCasesByPublishDate&metric=cumDeaths28DaysByDeathDate&format=csv"
+d1 <- read_csv(gov_uk) %>%
+    clean_names() %>%
+    select(date,
+           deaths28 = cum_deaths28days_by_death_date,
+           hospital_cases, 
+           new_admissions,
+           new_cases = new_cases_by_publish_date)
+
+save(d1, file = here("data", "raw", "contextual", "gov_d1.Rdata"))
+
+oxcgrt <- read_csv("https://raw.githubusercontent.com/OxCGRT/covid-policy-tracker/master/data/OxCGRT_latest.csv")
+
+url <- "https://covidtrackerapi.bsg.ox.ac.uk/api/v2/stringency/date-range/2020-04-01/2021-04-01"
+
+oxcgrt <- fromJSON(url)
+
+oxcgrt$data[[1]][[1]]
+
+
+oxcgrt %>%
+    filter(CountryName == "United Kingdom") %>%
+    clean_names() %>%
+    mutate(date = as_date(date))
+
+
+
