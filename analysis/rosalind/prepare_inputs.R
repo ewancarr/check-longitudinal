ca6de39dddb2629115cfbb154d69760dae572661
Ewan Carr <ewan.carr@kcl.ac.uk>
2021-05-05 11:24:44 +0100

    Update Mplus models; add R3STEP models
    This includes the first draft of models exploring
    baseline predictors of class membership.

--- analysis/rosalind/prepare_inputs.R
+++ analysis/rosalind/prepare_inputs.R
@@ -6,14 +6,25 @@ library(tidyverse)
 library(here)
 library(fs)
 library(MplusAutomation)
-
+library(fastDummies)
+library(naniar)
 load(here("data", "clean", "rep.Rdata"), verbose = TRUE)
+load(here("data", "clean", "baseline.Rdata"), verbose = TRUE)
 
 ###############################################################################
 ####                                                                      #####
 ####                        Prepare data for Mplus                        #####
 ####                                                                      #####
 ###############################################################################
+
+# # Decide which time points to use -------------------------------------------
+
+# Either all times points or 2-monthly questionnaires only.
+
+two_monthly <- FALSE
+if (two_monthly) {
+    sel <- filter(sel, two_month)
+}
 
 # Reshape outcomes to WIDE format ---------------------------------------------
 
@@ -31,26 +42,42 @@ wide_data <- sel %>%
     select(-dap) %>%
     spread(k, v)
 
-# Alternative: reshape outcomes to LONG format --------------------------------
-
-long_data <- sel %>%
-    select(pid, dap, age, female, is_staff, phq, gad)
-    
 # Add baseline variables ------------------------------------------------------
 
-bl <- sel %>%
-    select(pid, dap, age, female) %>%
-    arrange(pid, dap) %>%
-    group_by(pid) %>%
-    summarise(across(c(age, female), first))
+wide_data <- bl %>%
+    select(pid,
+           is_staff,
+           age,
+           female,
+           ethnic_f,
+           child6,
+           nc = numchild,
+           highrisk,
+           othercare,
+           shield_isol,
+           probdef,
+           kwself01,
+           livalon,
+           renting) %>%
+    mutate(eth = tolower(as.character(ethnic_f)),
+           othercare = othercare == "Yes") %>%
+    drop_na() %>%
+    dummy_cols(select_columns = c("eth", "nc")) %>%
+    select(-eth, -ethnic_f, -nc) %>%
+    full_join(wide_data, by = "pid")
 
-wide_data <- inner_join(bl, wide_data, by = "pid")
+# Remove characters Mplus doesn't like
+names(wide_data) <- str_replace_all(names(wide_data), "\\+", "")
 
 # Check: missing data?
 wide_data %>%
-    summarise(across(everything(), naniar::pct_complete)) %>%
+    summarise(across(everything(), pct_complete)) %>%
     gather(measure, pct_complete) %>%
     as.data.frame()
+
+# Create LONG version ---------------------------------------------------------
+
+long_data <- select(aw, phq = phq_total, gad = gad_total, pid, dap)
 
 # Export, create input file ---------------------------------------------------
 
@@ -69,20 +96,25 @@ names(input_file) <- c("wide", "long")
 
 ###############################################################################
 ####                                                                      #####
-####                          Create input files                          #####
+####              create input files: GROWTH MIXTURE MODELS               #####
 ####                                                                      #####
 ###############################################################################
 
 "
-For now, we're running:
+We're considering two types of model:
 
 1. Latent class growth analysis (LCGA)
 2. Growth mixture models
 
 The former is a simplified version of GMM, where within-class variations
-(variances and covariances) are constrained to zero. We'll fit models for
-cubic growth using (1) and (2) with increasing numbers of classes and examine
-model fit.
+(variances and covariances) are constrained to zero.
+
+And two functional forms:
+
+1. Quadratic
+2. Cubic
+
+However, for now, I'm just fitting GMM/cubic.
 "
 
 make_input <- function(dpath,
@@ -90,19 +122,19 @@ make_input <- function(dpath,
                        classes,
                        model,
                        form = "quadratic",
+                       proc = 2,
                        names_statement,
                        starts = FALSE,
-                       boot = FALSE) {
+                       boot = FALSE,
+                       r3step = FALSE) {
     # Get number of time points from input file
-
     dap <- str_split(names_statement, " ") %>%
         unlist() %>%
         map_chr(str_trim) %>%
-        keep(~ str_detect(.x, "[0-9]{2}$")) %>%
+        keep(~ str_detect(.x, "[gadphq][0-9]{2}$")) %>%
         map_dbl(parse_number) %>%
         unique() %>%
         sort() 
-
     # Get first/last time point
     start <- nth(dap, 1)
     end <- nth(dap, -1)
@@ -110,25 +142,32 @@ make_input <- function(dpath,
     # Get "NAMES" statement from input file
     vn <- str_wrap(str_squish(names_statement), 40)
     # Define right-hand side of growth curve
-    right_side <- str_wrap(str_squish(paste(y, pad(dap), "@", dap, sep = "", collapse = " ")), 40)
+    right_side <- str_wrap(str_squish(paste(y, pad(dap), "@.", pad(dap), sep = "", collapse = " ")), 40)
     # Define title
     title <- str_glue("{toupper(model)} model for {toupper(y)}, {classes} classes") 
     # Define 'use variables'
     uv <- str_glue("{y}{pad(start)}-{y}{pad(end)}")
     # Define plot SERIES
     series <- str_glue("{y}{pad(start)}-{y}{pad(end)}")
-    # Define STARTS and LRT bootstrap
-    st <- ifelse(starts, "\nSTARTS = 100 20;\n", "") 
+    # Define STARTS, PROCESSORS, LRT bootstrap
+    pr <- str_glue("\n\nPROCESSORS = {proc};\n")
+    st <- ifelse(starts, "\nSTARTS = 500 100;\n", "") 
     lrt <- ifelse(boot, "LRTBOOTSTRAP = 50;", "")
     tech14 = ifelse(boot, "TECH14", "")
     # Define functional form
     ff <- case_when(form == "quadratic" ~ "i s q",
-                    form == "cubic" ~ "i s q b")
+                    form == "cubic" ~ "i s q cu")
     # Define constraints
     constraints <- case_when(model == "lcga" & form == "quadratic" ~ "i-q@0",
-                             model == "lcga" & form == "cubic" ~ "i-b@0",
+                             model == "lcga" & form == "cubic" ~ "i-cu@0",
                              model == "gmm" & form == "quadratic" ~ "",
-                             model == "gmm" & form == "cubic" ~ "b@0")
+                             model == "gmm" & form == "cubic" ~ "cu@0")
+    # Define predictors (R3STEP)
+    if (r3step[1] > 1) {
+        r3step <- str_wrap(str_glue("AUXILIARY = {paste(r3step, '(R3STEP)', collapse = ' ')};\n"), 40)
+    } else {
+        r3step <- ""
+    }
     # Generate the model
     return(str_glue("
     TITLE: {title}
@@ -138,8 +177,9 @@ make_input <- function(dpath,
     USEVARIABLES = {uv};
     CLASSES = C({classes});
     IDVARIABLE = pid;
+    {r3step}
     MISSING=.;
-    ANALYSIS:
+    ANALYSIS: {pr}
     TYPE = MIXTURE; {st} {lrt}
     MODEL:
     %OVERALL%
@@ -155,12 +195,13 @@ make_input <- function(dpath,
 
 combinations <- list(dpath           = "../data/check_wide.dat",
                      y               = c("gad", "phq"),
-                     form            = c("quadratic", "cubic"),
+                     form            = "cubic",
                      names_statement = input_file$wide[4],
-                     classes         = 2:12,
-                     starts          = TRUE,
+                     classes         = 2:10,
+                     starts          = FALSE,
+                     proc            = 24,
                      boot            = FALSE,
-                     model           = c("lcga", "gmm")) %>%
+                     model           = "gmm") %>%
     cross()
 
 inputs <- map(combinations, ~ exec(make_input, !!!.x))
@@ -169,18 +210,55 @@ inputs <- map(combinations, ~ exec(make_input, !!!.x))
 inputs <- map2(inputs, 1:length(inputs),
                ~ paste0(.x, str_glue("\n\nSAVEDATA: \nFILE = save{.y}.dat;\nSAVE = CPROBABILITIES;")))
 
+
 # Save input files (and delete old version if exists)
-walk2(inputs, 1:length(inputs), function(m, f) {
-     fp <- here("analysis", "rosalind", "fits", f)
-     if (file_exists(fp)) {
-         file_delete(fp)
-     }
-     writeLines(m, fp)
-   }
-)
+write_models  <- function(inputs, stub = "", target) {
+    walk2(inputs, 1:length(inputs), function(m, f) {
+         fp <- paste0(target, "/", stub, f)
+         if (file_exists(fp)) {
+             file_delete(fp)
+         }
+         writeLines(m, fp)
+       })
+}
+
+write_models(inputs,
+             target = here("analysis", "rosalind", "local"))
+
 
 # Save index
 index <- reduce(combinations, bind_rows)
 index$model_id <- 1:length(combinations)
 save(index, file = here("analysis", "rosalind", "index.Rdata"))
 
+###############################################################################
+####                                                                      #####
+####          create input files: PREDICTORS OF CLASS MEMBERSHIP          #####
+####                                                                      #####
+###############################################################################
+
+# Pick base models ------------------------------------------------------------
+pick <- combinations %>%
+    # Pick base models
+    keep(~ (.x$y == "gad" & .x$classes == 6) | 
+           (.x$y == "phq" & .x$classes == 5)) 
+
+# Define sets of covariates ---------------------------------------------------
+to_include <- list(a = "age", 
+                   b = "female",
+                   c = "child6",
+                   d = "livlon",
+                   e = "renting",
+                   f = "kwself01")
+to_include$g <- reduce(to_include, c) 
+
+# Create combinations of models/covariates ------------------------------------
+
+inputs2 <- cross(list(mod = pick,
+           r3step = to_include)) %>%
+    map(~ flatten(list_merge(.x[1], .x[2]))) %>%
+    map(~ exec(make_input, !!!.x))
+
+write_models(inputs2,
+             stub = "r3step",
+             target = here("analysis", "rosalind", "local"))
