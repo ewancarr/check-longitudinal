8d633b4c882241543db615b6c86324a26c2e1cad
Ewan Carr <ewan.carr@kcl.ac.uk>
2021-05-21 09:58:45 +0100

    Update batch script

--- analysis/mplus/run.sh
+++ analysis/mplus/run.sh
@@ -3,10 +3,16 @@
 #SBATCH --tasks=4
 #SBATCH --mem=4000
 #SBATCH --job-name=array
-#SBATCH --array=1-62
+#SBATCH --array=1
 #SBATCH --output=/scratch/users/%u/check-longitudinal/analysis/mplus/logs/%a.out
 #SBATCH --time=0-96:00
 module load apps/singularity
-cd /scratch/users/k1644956/check-longitudinal/analysis/mplus/input_files
-inp=$(sed "$SLURM_ARRAY_TASK_ID q;d" ../data/index)
-singularity exec ../../../containers/mplus.simg mplus $inp
+MPLUS='singularity exec /scratch/users/k1644956/check-longitudinal/containers/mplus.simg mplus'
+cd /scratch/users/k1644956/check-longitudinal/analysis/mplus
+INP=input_files/$(sed "$SLURM_ARRAY_TASK_ID q;d" data/index)
+FOLDER=$(echo $INP | cut -f1 -d ' ')
+FILE=$(echo $INP | cut -f2 -d ' ')
+cd $FOLDER
+$MPLUS $FILE
+
+
