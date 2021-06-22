476dd696f95701f0ed6719e8412e3f6c8032ac6a
Ewan Carr <ewan.carr@kcl.ac.uk>
2021-06-22 09:40:03 +0100

    Function to extract odds ratios from R3STEP multinomial regression

--- analysis/functions/extract_multinomial.R
+++ analysis/functions/extract_multinomial.R
@@ -3,7 +3,7 @@ library(here)
 
 extract_r3step <- function(p) {
 
-    # Load the output file
+    # Load the output file ----------------------------------------------------
 
     entire_file <- readLines(p)
 
@@ -20,38 +20,72 @@ extract_r3step <- function(p) {
         return(NA)
     }
 
-    # Extract the relevant section from the output file
-
+    # Select required sections ------------------------------------------------
+    # 1: Odds ratios and p-values              NOTE: Not using this at present.
     start <- grep(starting_string, entire_file)
     end <- grep(ending_string, entire_file)
-    results <- entire_file[(start):(end - 1)]
+    A <- entire_file[(start + 5):(end - 1)]
 
-    # Extract each "Parameterization using..." section
+    # 2: Confidence intervals
+    start <- grep("CONFIDENCE INTERVALS OF ODDS RATIOS FOR TESTS OF CATEGORICAL LATENT VARIABLE MULTINOMIAL",
+                  entire_file)
+    end <- min(grep("TECHNICAL 1 OUTPUT|SAMPLE STATISTICS FOR ESTIMATED FACTOR SCORES", entire_file))
+    ci <- entire_file[(start + 2):(end - 1)]
 
+    # Extract confidence intervals
     pos <- c(1,
-             grep("^Parameterization using Reference Class.*$", results),
-             length(results))
-    results <- split(results, rep(seq(pos), diff(c(pos, length(results) + 1))))
-    results <- results[-c(1, length(results))]
-    names(results) <- map_dbl(results, ~ parse_number(.x[1]))
+             grep("^Parameterization using Reference Class.*$", ci),
+             length(ci))
+    ci <- split(ci,
+                rep(seq(pos),
+                    diff(c(pos, length(ci) + 1))))  %>%
+         discard(~ length(.x) < 3) %>%
+         map(~ .x[2:length(.x)])
+
+    ci <- map_dfr(ci, function(i) {
+                  i <- i[i != ""]
+                  i <- data.frame(i) %>%
+                     mutate(class = str_extract(i, "^ *C#\\d+")) %>%
+                     fill(class, .direction = "down") %>%
+                     separate(i, 
+                              c("drop", "param", "low005", "low025", "low05",
+                                "est", "up05", "up025", "up005"),
+                              sep = " +",
+                              fill = "right") %>%
+                     select(-drop) %>%
+                     drop_na(est) 
+                 class <- unique(parse_number(i$class))
+                 i$ref <- paste0("C#", setdiff(1:(length(class) + 1), class))
+                 i$param <- tolower(i$param)
+                 return(i)
+                     }
+    )
+
+    ci <- ci %>%
+        mutate(across(everything(),
+                      ~ if_else(str_detect(.x, "^\\*\\*\\*\\*+"),
+                                NA_character_, .x)))
+
+    return(ci)
+}
 
     # Convert strings into a tidy data frame
 
-    return(map_dfr(results, function(x) {
-        as.data.frame(x[-1]) %>%
-            set_names("row") %>%
-            mutate(class = str_extract(row, "^ *C#\\d+"),
-                   to_drop = row == "" | !is.na(class)) %>%
-            fill(class, .direction = "down") %>%
-            filter(!to_drop) %>%
-            separate(row,
-                     letters[1:6],
-                     sep = "[ ]+",
-                     extra = "drop",
-                     fill = "right") %>%
-            drop_na(c) %>%
-            select(param = b, on = class, est = c, se = d, pval = f)
-             }, .id = "reference") %>%
-        mutate(across(est:pval, as.numeric),
-               or = exp(est)))
-}
+    # return(map_dfr(results, function(x) {
+    #     as.data.frame(x[-1]) %>%
+    #         set_names("row") %>%
+    #         mutate(class = str_extract(row, "^ *C#\\d+"),
+    #                to_drop = row == "" | !is.na(class)) %>%
+    #         fill(class, .direction = "down") %>%
+    #         filter(!to_drop) %>%
+    #         separate(row,
+    #                  letters[1:6],
+    #                  sep = "[ ]+",
+    #                  extra = "drop",
+    #                  fill = "right") %>%
+    #         drop_na(c) %>%
+    #         select(param = b, on = class, est = c, se = d, pval = f)
+    #          }, .id = "reference") %>%
+    #     mutate(across(est:pval, as.numeric),
+    #            or = exp(est)))
+
