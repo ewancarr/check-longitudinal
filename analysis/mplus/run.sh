f79433e0c9feb36b10dee8827c9cc9535ae970ba
Ewan Carr <ewan.carr@kcl.ac.uk>
2021-06-24 19:01:03 +0100

    Minor edit to SLURM script

--- analysis/mplus/run.sh
+++ analysis/mplus/run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH --partition=brc
-#SBATCH --tasks=4
-#SBATCH --mem=4000
-#SBATCH --job-name=array
-#SBATCH --array=63-98
+#SBATCH --tasks=8
+#SBATCH --mem=8000
+#SBATCH --job-name=ch_long
+#SBATCH --array=0-92
 #SBATCH --output=/scratch/users/%u/check-longitudinal/analysis/mplus/logs/%a.out
-#SBATCH --time=0-200:00
+#SBATCH --time=0-99:00
 module load apps/singularity
 MPLUS='singularity exec /scratch/users/k1644956/check-longitudinal/containers/mplus.simg mplus'
 cd /scratch/users/k1644956/check-longitudinal/analysis/mplus
